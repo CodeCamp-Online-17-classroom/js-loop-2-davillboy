cdcda5b049858088f62336cff026735e96c6c1e7
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 13:57:06 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // write code here
+ <script>
+        let sum = 0 ;
+        while (true ) {
+            let v = +prompt("Enter Number","")
+            if (!v) {break}
+            sum += v  // sum = sum + v
+        }
+        console.log("sum : " + sum)
+
+    </script>
